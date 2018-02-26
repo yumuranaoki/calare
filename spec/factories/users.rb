72cefe1492da2c867617f5caf0087a5f6ffa9adc
Naoki Yumura <naokiyumura@NaokinoMacBook-ea.local>
2018-02-27 00:39:14 +0900

    add test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    name "tanaka takeshi"
+    email "tanakatakeshi@example.com"
+    password "hogehoge"
+    password_confirmation "hogehoge"
   end
 end
