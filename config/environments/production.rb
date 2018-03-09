7d4bb40a6d439b4a8008ad316fe5217d42bf3ce4
Naoki Yumura <naokiyumura@NaokinoMacBook-ea.local>
2018-03-09 23:53:23 +0900

    productionのmail用のhost変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   #awsで取得したdomain
-  host = 'calare-schedule.com'
+  host = 'www.calare-schedule.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.gmail.com',
