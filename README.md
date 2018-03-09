279fac571e5ee6da9eb8134b87b913c39b2a05d1
Naoki Yumura <naokiyumura@NaokinoMacBook-ea.local>
2018-03-09 23:33:18 +0900

    modify ReadMe

--- README.md
+++ README.md
@@ -7,4 +7,6 @@
 * カレンダー画面をドラッグして簡単に日程を提案　　
 * google calendarと連携して空いている日程を取得して提案　　
 * 本アプリを通して日程調整に回答する際もgoogle calendarの空いている日程をワンクリックで選択できる　　
-* ２人でも複数人でも対応　　
+* ２人でも複数人でも対応　
+
+[https://www.calare-schedule.com/](https://www.calare-schedule.com/)　
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   #awsで取得したdomain
-  host = '54.65.64.54'
+  host = 'calare-schedule.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.gmail.com',
